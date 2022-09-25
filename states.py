75103df122ed0bea2436e73294ec482a8d8afc33
David Naneishvili <davnub@yandex.ru>
2022-09-25 23:40:16 +0400

    added state reset in states.py, also added a state 'pick_city' to Lowprice states

--- states.py
+++ states.py
@@ -25,9 +25,15 @@ class Lowprice(StatesGroup):
     check_out_date = State()
     city = State()
     validate_city = State()
+    pick_city = State()
     amount_hotels = State()
     if_photos = State()
     how_many_photos = State()
 
 
 bot.add_custom_filter(custom_filters.StateFilter(bot))
+
+
+@bot.message_handler(state="*", commands=['cancel'])
+def reset_state(message):
+    bot.send_message(message.from_user.id, "Вернулись в начало.\nВведи /help и я подскажу что да как:)")
