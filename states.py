9e34e751386a44b4a1e57b92cc6b839beafdffd3
David Naneishvili <davnub@yandex.ru>
2022-09-28 23:43:15 +0400

    removed redundant states

--- states.py
+++ states.py
@@ -3,23 +3,6 @@ from loader import bot
 from telebot import custom_filters
 
 
-class UserState(StatesGroup):
-    city = State()
-    amount_hotels = State()
-    if_photos = State()
-    how_many_photos = State()
-    price_range = State()
-    distance_range = State()
-    check_in_date = State()
-    check_out_date = State()
-
-
-class QueryState(StatesGroup):
-    lowprice = State()
-    highprice = State()
-    bestdeal = State()
-
-
 class Lowprice(StatesGroup):
     check_in_date = State()
     check_out_date = State()
