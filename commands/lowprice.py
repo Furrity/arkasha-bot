9f14ebd101012bde120f0fa94514700a7b3288d6
David Naneishvili <davnub@yandex.ru>
2022-09-28 23:53:22 +0400

    reformatted lowprice, moved utility methods to its module

--- commands/lowprice.py
+++ commands/lowprice.py
@@ -2,8 +2,8 @@ from typing import List
 from telebot import types
 from states import Lowprice
 from loader import bot
-from datetime import datetime
 import api
+from utilities import *
 
 
 def check_in(message: types.Message):
@@ -231,37 +231,3 @@ def give_result(user_id: int, chat_id: int):
                 bot.send_media_group(user_id,
                                      [types.InputMediaPhoto(photo_link) for photo_link in hotel['photo_links']],
                                      chat_id)
-
-
-def valid_date(date_text):
-    try:
-        datetime.strptime(date_text, '%d-%m-%Y')
-        return True
-    except ValueError:
-        return False
-
-
-def reformat_date(date_text):
-    """Изменит формат из дд-мм-гггг в гггг-мм-дд"""
-    date_list = date_text.split('-')
-    new_date = '-'.join(date_list[::-1])
-    return new_date
-
-
-def date_passed(date_text):
-    present = datetime.now().strftime('%Y-%m-%d')
-    return first_date_later_that_second(present, date_text)
-
-
-def first_date_later_that_second(date_text_1, date_text_2):
-    d1 = datetime.strptime(date_text_1, '%Y-%m-%d').date()
-    d2 = datetime.strptime(date_text_2, '%Y-%m-%d').date()
-    return d2 < d1
-
-
-def make_options_to_send(options: List[dict]):
-    result_list = []
-    for i_option in range(len(options)):
-        result_list.append(str(i_option + 1) + ' ' + options[i_option]['name'])
-    result = '\n'.join(result_list)
-    return result
